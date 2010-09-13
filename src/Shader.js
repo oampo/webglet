799301785a12f4dfa4222c973ef8ef6c5a0d05a0
Joe Turner <joe@eli>
2010-09-13 14:02:44 +0100

    Add basic shader code

--- src/Shader.js
+++ src/Shader.js
@@ -1,4 +1,29 @@
+
+
 var Shader = new Class({
-    initialize: function() {
+    initialize: function(shaderId) {
+        this.name = shaderId;
+        var sourceElement = $(this.name);
+        var typeString = sourceElement.get('type');
+        if (typeString == 'x-shader/x-vertex') {
+            this.type = gl.VERTEX_SHADER;
+        }
+        else if (typeString == 'x-shader/x-fragment') {
+            this.type = gl.FRAGMENT_SHADER;
+        }
+
+        this.shader = gl.createShader(this.type);
+
+        gl.shaderSource(this.shader, sourceElement.get('text'));
+        gl.compileShader(this.shader);
+
+        if (!gl.getShaderParameter(this.shader, gl.COMPILE_STATUS)) {
+            console.error('Could not compile shader ', this.name, '\n',
+                          gl.getShaderInfoLog(this.shader));
+        }
+    },
+
+    getShader: function() {
+        return (this.shader);
     }
 });
