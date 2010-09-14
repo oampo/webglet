d03f51537161725aae2e160790fc2acb61e0661c
Joe Turner <joe@eli>
2010-09-14 16:16:11 +0100

    Make mesh access buffer modes correctly

--- src/Mesh.js
+++ src/Mesh.js
@@ -12,9 +12,9 @@ var Mesh = new Class({
         this.colorUsage = colorUsage;
         this.normalUsage = normalUsage;
 
-        this.vertexBuffer = new Buffer(numVertices, this.options.vertexUsage);
-        this.normalBuffer = new Buffer(numVertices, this.options.normalUsage);
-        this.colorBuffer = new Buffer(numVertices, this.options.colorUsage);
+        this.vertexBuffer = new Buffer(numVertices, this.vertexUsage);
+        this.normalBuffer = new Buffer(numVertices, this.normalUsage);
+        this.colorBuffer = new Buffer(numVertices, this.colorUsage);
 
         this.position = [0, 0, 0];
         this.rotation = [0, 0, 0];
