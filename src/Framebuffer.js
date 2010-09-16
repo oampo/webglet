675e24652fe70daaa963dbaeed4cd137acf7e60a
Joe Turner <joe@eli>
2010-09-16 13:54:41 +0100

    Add untested code for render-to-texture

---
+++ src/Framebuffer.js
@@ -0,0 +1,45 @@
+/**
+ * @depends App.js
+ * @depends Texture.js
+ */
+
+var Framebuffer = new Class({
+    initialize: function(width, height) {
+        this.framebuffer = gl.createFramebuffer();
+        
+        this.begin();
+
+        // Add depth buffer
+        this.depthBuffer = gl.createRenderbuffer();
+        gl.bindRenderbuffer(gl.RENDERBUFFER, this.depthBuffer);
+        gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, width,
+                               height);
+        gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT,
+                                   gl.RENDERBUFFER, this.depthBuffer);
+        gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+
+        // Add texture for color
+        this.texture = new Texture(width, height);
+        this.texture.begin();
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT,
+                                gl.TEXTURE_2D, texture.getTexture(),
+                                0);
+        
+        // Check it all worked
+        if (gl.checkFrameBufferStatus(gl.FRAMEBUFFER) !=
+            gl.FRAMEBUFFER_COMPLETE) {
+            console.error("Could not create framebuffer");
+        }
+
+        this.end();
+    },
+
+    begin: function() {
+        gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebuffer)
+    },
+
+    end: function() {
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    },
+
+});
