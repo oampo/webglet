0c347a130dceb108f3c3ca8807b6662f057c9979
Joe Turner <joe@eli>
2010-09-16 15:19:44 +0100

    Add slightly ropey looking texture support

---
+++ src/Texture.js
@@ -0,0 +1,30 @@
+/**
+ * @depends App.js
+ */
+
+ var Texture = new Class({
+    initialize: function(width, height) {
+        this.texture = gl.createTexture();
+        this.bind();
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,  width, height, 0, gl.RGBA,
+                      gl.UNSIGNED_BYTE);
+        this.end();
+    },
+
+    begin: function(textureUnit) {
+        gl.activeTexture(gl.TEXTURE0 + textureUnit);
+        this.bind();
+    },
+
+    bind: function() {
+        gl.bindTexture(gl.TEXTURE_2D, this.texture);
+    },
+
+    end: function() {
+        gl.bindTexture(gl.TEXTURE_2D, null);
+    },
+
+    getTexture: function() {
+        return (this.texture);
+    }
+});
