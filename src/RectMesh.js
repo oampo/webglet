f2e6e09501780c443695fec8d25f396e0a1f4df6
Joe Turner <joe@eli>
2010-09-24 12:32:46 +0100

    Make RectMesh use TRIANGLE_STRIP so colors and normals make more sense

--- src/RectMesh.js
+++ src/RectMesh.js
@@ -8,22 +8,18 @@ var RectMesh = new Class({
     Extends: Mesh,
     initialize: function(width, height, vertexUsage, colorUsage, normalUsage,
                          texCoordUsage) {
-        this.parent(6, gl.TRIANGLES, vertexUsage, colorUsage, normalUsage,
+        this.parent(4, gl.TRIANGLE_STRIP, vertexUsage, colorUsage, normalUsage,
                     texCoordUsage);
 
         this.vertexBuffer.setValues([0,     0,      0,
-                                     width, height, 0,
                                      0,     height, 0,
-                                     0,     0,      0,
-                                     width, height, 0, 
-                                     width, 0,      0]);
+                                     width,     0, 0,
+                                     width, height, 0]);
         if ($chk(this.texCoordUsage)) {
             this.texCoordBuffer.setValues([0, 1,
-                                           1, 0,
                                            0, 0,
-                                           0, 1,
-                                           1, 0,
-                                           1, 1]);
+                                           1, 1,
+                                           1, 0]);
         }
     }
 });
