02911392a919f907aa526d161704fa7ab57c3aa4
Joe Turner <joe@oampo.co.uk>
2010-10-12 16:45:40 +0100

    Make texture wrap using CLAMP_TO_EDGE so we can use npot textures in chrome

--- src/Texture.js
+++ src/Texture.js
@@ -8,6 +8,8 @@
         this.bind();
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA,
                       gl.UNSIGNED_BYTE, null);
         this.end();
