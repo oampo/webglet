b989e89a3f3715a4538a29b006a5ce41910c401d
Joe Turner <joe@oampo.co.uk>
2010-10-14 12:50:12 +0100

    Change NEAREST texture filter to LINEAR.  Looks nicer :)

--- src/Texture.js
+++ src/Texture.js
@@ -6,8 +6,8 @@
     initialize: function(width, height) {
         this.texture = gl.createTexture();
         this.bind();
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA,
