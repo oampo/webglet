3c93ba40452d32716c12ef7289f7a16398307e87
Joe Turner <joe@oampo.co.uk>
2010-12-01 16:04:24 +0000

    Get rid of TransformationMatrices class.  It didn't really do anything except make the matrix and uniform handling less transparent

--- src/TransformationMatrices.js
+++
@@ -1,9 +0,0 @@
-/**
- * @depends MatrixStack.js
- */
-var TransformationMatrices = new Class({
-    initialize: function() {
-        this.projection = new MatrixStack();
-        this.modelview = new MatrixStack();
-    }
-});
