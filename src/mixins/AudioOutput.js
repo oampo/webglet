20cccab42ede3120f3a8622eda8202e3db8afbb6
Joe Turner <joe@oampo.co.uk>
2011-01-10 10:43:22 +0000

    Change audio output to use variable length buffers

--- src/mixins/AudioOutput.js
+++ src/mixins/AudioOutput.js
@@ -1,8 +1,8 @@
 var AudioOutput = new Class({
     initAudioOutput: function(numChannels, bufferSize, sampleRate) {
         this.numChannels = numChannels || 2;
-        this.bufferSize = bufferSize || 32784;
         this.sampleRate = sampleRate || 44100.0;
+        this.bufferSize = bufferSize || 32784;
 
         this.output = new Audio();
         this.overflow = null;
@@ -10,11 +10,20 @@ var AudioOutput = new Class({
 
         if (typeof this.output.mozSetup === 'function' ) {
             this.output.mozSetup(this.numChannels, this.sampleRate);
-            this.fillBuffer.periodical(1000 * bufferSize / sampleRate, this);
+            this.audioEnabled = true;
+        }
+        else {
+            this.audioEnabled = false;
+            // Allow profiling in chrome
+            this.output.mozCurrentSampleOffset = function() {
+                return 0;
+            };
+            this.output.mozWriteAudio = function() {
+            };
         }
     },
 
-    fillBuffer: function() {
+    tickAudioOutput: function() {
         // Check if some data was not written in previous attempts
         var numSamplesWritten;
         if (this.overflow) {
@@ -31,28 +40,22 @@ var AudioOutput = new Class({
 
         // Check if we need add some data to the audio output.
         var outputPosition = this.output.mozCurrentSampleOffset();
-        var samplesNeeded;
-        if (outputPosition == 0) {
-            samplesNeeded = this.bufferSize;
-        }
-        else {
-            samplesNeeded = outputPosition + this.bufferSize - this.writePosition;
-        }
+        var samplesNeeded = outputPosition + this.bufferSize - this.writePosition;
         if (samplesNeeded > 0) {
             // Request some sound data from the callback function.
-            var audioData = this.audioRequested();
+            var buffer = this.audioRequested(Math.floor(samplesNeeded));
 
             // Writing the data.
-            numSamplesWritten = this.output.mozWriteAudio(audioData);
-            if (numSamplesWritten < audioData.length) {
+            numSamplesWritten = this.output.mozWriteAudio(buffer);
+            if (numSamplesWritten < buffer.length) {
                 // Not all the data was written, saving the tail.
-                this.overflow = audioData.slice(numSamplesWritten);
+                this.overflow = buffer.slice(numSamplesWritten);
             }
             this.writePosition += numSamplesWritten;
         }
     },
 
     // Override me!
-    audioRequested: function() {
+    audioRequested: function(samplesNeeded) {
     }
 });
