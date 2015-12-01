4dd853383d5cc7aad4768f7b64fdf438495db001
Joe Turner <joe@oampo.co.uk>
2015-12-01 12:57:24 +0000

    Switch to webpack.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+var webpack = require('webpack');
+
+var packageData = require('./package.json');
+
+module.exports = {
+    entry: path.resolve(__dirname, packageData.main),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: [packageData.name, packageData.version, 'js'].join('.'),
+        libraryTarget: 'var',
+        library: packageData.name
+    },
+    devtool: 'source-map',
+    module: {
+      loaders: [
+        {
+          test: /\.js?$/,
+          exclude: /(node_modules)/,
+          loader: 'babel' // 'babel-loader' is also a legal name to reference
+        }
+      ]
+    }
+}
