78f5c074005a433e7b46ac3e577e0976781fbc05
Joe Turner <joe@oampo.co.uk>
2015-12-02 11:51:26 +0000

    Switch to single webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 var path = require('path');
 
+var webpack = require('webpack');
+
 var packageData = require('./package.json');
+
+var minify = process.argv.indexOf('--minify') != -1;
+
+var filename = [packageData.name, packageData.version, 'js'];
+var plugins = [];
+
+if (minify) {
+    filename.splice(filename.length - 1, 0, 'min');
+    plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
 
 module.exports = {
     entry: path.resolve(__dirname, packageData.main),
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: [packageData.name, packageData.version, 'js'].join('.'),
+        filename: filename.join('.'),
         libraryTarget: 'var',
         library: packageData.name
     },
@@ -19,5 +31,6 @@ module.exports = {
           loader: 'babel' // 'babel-loader' is also a legal name to reference
         }
       ]
-    }
-}
+    },
+    plugins: plugins
+};
